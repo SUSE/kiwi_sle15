dca26fff9b5873450529cb323f534de1b59676d9
Dan Čermák <dcermak@suse.com>
2024-01-19 14:06:51 +0100

    Quote shell variables to prevent word splitting
    this fixes shellcheck warning SC2086
    https://www.shellcheck.net/wiki/SC2086
    
    (cherry picked from commit 3436964396fa8fb3b494436e403d09a4fb97c67d)

--- dracut/modules.d/90kiwi-dump/kiwi-dump-image.sh
+++ dracut/modules.d/90kiwi-dump/kiwi-dump-image.sh
@@ -84,7 +84,7 @@ function get_disk_list {
           disk_device="$(echo "${disk_mpath_child}" | cut -f1 -d:)"
         fi
         disk_size=$(echo "${disk_meta}" | cut -f2 -d:)
-        if [ ${max_disk} -gt 0 ]; then
+        if [ "${max_disk}" -gt 0 ]; then
             local disk_size_bytes
             disk_size_bytes=$(binsize_to_bytesize "${disk_size}") || \
                 disk_size_bytes=0
--- dracut/modules.d/99kiwi-lib/kiwi-filesystem-lib.sh
+++ dracut/modules.d/99kiwi-lib/kiwi-filesystem-lib.sh
@@ -110,7 +110,7 @@ function probe_filesystem {
     if [ "${fstype}" = "crypto_LUKS" ];then
         fstype=luks
     fi
-    echo ${fstype}
+    echo "${fstype}"
 }
 
 #======================================
