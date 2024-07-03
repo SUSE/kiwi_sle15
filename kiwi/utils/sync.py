0b501b70de9e8cf9d7d0a2460e81b15b386bb379
Marcus Schäfer <marcus.schaefer@suse.com>
2024-07-03 14:45:56 +0200

    Delete (py)xattr module requirement
    The required functionality is provided by the core os library

--- kiwi/utils/sync.py
+++ kiwi/utils/sync.py
@@ -16,10 +16,10 @@
 # along with kiwi.  If not, see <http://www.gnu.org/licenses/>
 #
 import os
+import errno
 import logging
 from stat import ST_MODE
 from typing import List
-import xattr
 
 # project
 from kiwi.command import Command
@@ -134,9 +134,11 @@ class DataSync:
         :rtype: bool
         """
         try:
-            xattr.getxattr(self.target_dir, 'user.mime_type')
-        except Exception as e:
-            if format(e).startswith('[Errno 95]'):
-                # libc interface [Errno 95] Operation not supported:
+            os.getxattr(self.target_dir, 'user.mime_type')
+        except OSError as e:
+            log.debug(
+                f'Check for extended attributes on {self.target_dir} said: {e}'
+            )
+            if e.errno == errno.ENOTSUP:
                 return False
         return True
